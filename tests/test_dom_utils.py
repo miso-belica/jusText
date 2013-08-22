f2ba250a48c7e10c437383b92cd7415d153bb896
Mišo Belica <miso.belica@gmail.com>
2013-08-22 11:06:51 +0200

    100% test coverage of function 'preprocess'

--- tests/test_dom_utils.py
+++ tests/test_dom_utils.py
@@ -7,7 +7,7 @@ import unittest
 
 from nose import tools
 from lxml import html
-from justext.core import remove_comments, remove_tags
+from justext.core import preprocess, remove_comments, remove_tags
 
 
 class TestDomUtils(unittest.TestCase):
@@ -99,5 +99,97 @@ class TestDomUtils(unittest.TestCase):
             '<p>pre<span>text</span>postpopost</p>'
             '<p>footer  a boss</p>'
             '</body></html>'
+        )
+        tools.assert_equal(expected, returned)
+
+    def test_preprocess_simple_unicode_string(self):
+        html_string = (
+            '<html><head><title>Title</title></head><body>'
+            '<h1>Header</h1>'
+            '<p>pre<span>text</span>post<em>emph</em>popost</p>'
+            '<p>footer <em>like</em> a boss</p>'
+            '</body></html>'
+        )
+
+        dom = preprocess(html_string)
+        returned = html.tostring(dom).decode("utf8")
+        expected = (
+            '<html><body>'
+            '<h1>Header</h1>'
+            '<p>pre<span>text</span>post<em>emph</em>popost</p>'
+            '<p>footer <em>like</em> a boss</p>'
+            '</body></html>'
+        )
+        tools.assert_equal(expected, returned)
+
+    def test_preprocess_simple_bytes_string(self):
+        html_string = (
+            b'<html><head><title>Title</title></head><body>'
+            b'<h1>Header</h1>'
+            b'<p>pre<span>text</span>post<em>emph</em>popost</p>'
+            b'<p>footer <em>like</em> a boss</p>'
+            b'  <!-- abcdefgh -->\n'
+            b'</body></html>'
+        )
+
+        dom = preprocess(html_string)
+        returned = html.tostring(dom).decode("utf8")
+        expected = (
+            '<html><body>'
+            '<h1>Header</h1>'
+            '<p>pre<span>text</span>post<em>emph</em>popost</p>'
+            '<p>footer <em>like</em> a boss</p>'
+            '  \n'
+            '</body></html>'
+        )
+        tools.assert_equal(expected, returned)
+
+    def test_preprocess_simple_unicode_xhtml_string_with_declaration(self):
+        html_string = (
+            '<?xml version="1.0" encoding="windows-1250"?>'
+            '<!DOCTYPE html>'
+            '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="sk" lang="sk">'
+            '<head>'
+            '<title>Hello World</title>'
+            '<meta http-equiv="imagetoolbar" content="no" />'
+            '<meta http-equiv="Content-Type" content="text/html; charset=windows-1250" />'
+            '</head>'
+            '<body id="index">'
+            '</body>'
+            '</html>'
+        )
+
+        dom = preprocess(html_string)
+        returned = html.tostring(dom).decode("utf8")
+        expected = (
+            '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="sk" lang="sk">'
+            '<body id="index">'
+            '</body>'
+            '</html>'
+        )
+        tools.assert_equal(expected, returned)
+
+    def test_preprocess_simple_bytes_xhtml_string_with_declaration(self):
+        html_string = (
+            b'<?xml version="1.0" encoding="windows-1250"?>'
+            b'<!DOCTYPE html>'
+            b'<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="sk" lang="sk">'
+            b'<head>'
+            b'<title>Hello World</title>'
+            b'<meta http-equiv="imagetoolbar" content="no" />'
+            b'<meta http-equiv="Content-Type" content="text/html; charset=windows-1250" />'
+            b'</head>'
+            b'<body id="index">'
+            b'</body>'
+            b'</html>'
+        )
+
+        dom = preprocess(html_string)
+        returned = html.tostring(dom).decode("utf8")
+        expected = (
+            '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="sk" lang="sk">'
+            '<body id="index">'
+            '</body>'
+            '</html>'
         )
         tools.assert_equal(expected, returned)
