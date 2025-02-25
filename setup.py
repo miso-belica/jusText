9fb3340ad2087110348de513e4a4b6fd4f3cc839
Mišo Belica <1072330+miso-belica@users.noreply.github.com>
2025-02-25 20:17:46 +0000

    Bump version: 3.0.1 → 3.0.2

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open("README.rst") as readme:
 
 setup(
     name="jusText",
-    version="3.0.1",
+    version="3.0.2",
     description="Heuristic based boilerplate removal tool",
     long_description=long_description,
     author="Jan Pomikálek",
